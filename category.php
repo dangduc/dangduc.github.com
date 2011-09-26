1308b5c75dc0accdaaed080038bcc742b9c35fbb
Duc Nguyen <me@dangduc.com>
2011-09-25 22:51:10 -0700

    added categories

--- category.php
+++ category.php
@@ -1,13 +1,14 @@
 <?php get_template_part('tophead'); ?>
-   <?php $categories = get_categories(array( 'hide_empty'=>1)); ?>
-   <?php foreach($categories as $cat): ?>
+      <?php 
+      $categories = get_the_category();
+      foreach($categories as $cat): ?>
       <div class="category"><span><a href="<?php echo get_bloginfo('home').'?cat='.$cat->cat_ID;?>"><?php echo $cat->cat_name; ?></a></span></div>
+
+      <?php endforeach; ?>
       <div class="posts">
-         <?php $posts_array = get_posts("category=".$cat->cat_ID); ?>
-         <?php foreach( $posts_array as $post ) :	setup_postdata($post); ?>
+         <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
          <div class="post" id="post-<?php the_ID(); ?>">
             <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><?php the_title(); ?></a><?php edit_post_link("Edit", " ", ""); ?></h1>
-            
             <div class="entry">
                <?php the_excerpt(); ?>
                <div class="attributes">
@@ -15,9 +16,13 @@
                </div>  <!-- end attributes -->       
             </div> <!-- end entry -->
          </div> <!-- end post -->
-         <?php endforeach; ?> <!-- end foreach of posts for a category -->
+         <?php endwhile; ?> <!-- end foreach of post for a category -->
       </div> <!-- end posts -->
-   <?php endforeach; ?> <!-- end foreach of categories -->
+   <?php else : ?>
+      <div class="post">
+         <h2>Not Found</h2>
+      </div>
+   <?php endif; ?>
    <div id="footer">Duc Dang, 2010</div>
 
 </body>
--- single.php
+++ single.php
@@ -1,6 +1,14 @@
 <?php get_template_part('tophead'); ?>
+
+<?php 
+$categories = get_the_category();
+foreach($categories as $cat): ?>
+<div class="category"><span><a href="<?php echo get_bloginfo('home').'?cat='.$cat->cat_ID;?>"><?php echo $cat->cat_name; ?></a></span></div>
+
+<?php endforeach; ?>
+
 <div class="posts">
- 
+   
    <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
       <div class="post" id="post-<?php the_ID(); ?>">
          <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><?php the_title(); ?></a><?php edit_post_link("Edit", " ", ""); ?></h1>
@@ -26,7 +34,7 @@
    <?php endif; ?>
 </div> <!-- end posts -->
 
-
+<div id="footer">Duc Dang, 2010</div>
 
 </body>
 
