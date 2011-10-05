3f5f124a5644be640642603246adbfbe98562a67
Duc Nguyen <me@dangduc.com>
2011-10-05 01:48:07 -0700

    Fix for broken img url on single pages

--- tophead.php
+++ tophead.php
@@ -27,13 +27,13 @@
 <div id="menu">
    <ul>
       <li> 
-         <div class="icon"><a href="<?php bloginfo('url'); ?>"><img src="./wp-content/themes/superman7/images/home.png"/><span>Home</span></a></div>
+         <div class="icon"><a href="<?php bloginfo('url'); ?>"><img src="http://www.dangduc.com/wp-content/themes/superman7/images/home.png"/><span>Home</span></a></div>
       </li>
       <li>  
-         <div class="icon"><a href="<?php bloginfo('url'); ?>/about-me/"><img src="./wp-content/themes/superman7/images/about.png"/><span>About Me</span></a></div>
+         <div class="icon"><a href="<?php bloginfo('url'); ?>/about-me/"><img src="http://www.dangduc.com/wp-content/themes/superman7/images/about.png"/><span>About Me</span></a></div>
       </li>
       <li>  
-         <div class="icon"><a href="http://github.com/dangduc/"><img src="./wp-content/themes/superman7/images/github.png"/><span>GitHub</span></a></div>
+         <div class="icon"><a href="http://github.com/dangduc/"><img src="http://www.dangduc.com/wp-content/themes/superman7/images/github.png"/><span>GitHub</span></a></div>
       </li>
    </ul>
 </div>
